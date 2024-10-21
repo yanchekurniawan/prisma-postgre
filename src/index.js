6a51bf8fbe516824e802f480396ff90f6ecd50ce
Yanche Kurniawan Mangalik <yanche.k.m@gmail.com>
2024-10-21 15:16:47 +0800

    a

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.post("/products", async (req, res) => {
+  const { name, price, description } = req.body;
+  await prisma.product.create({
+    data: {
+      name,
+      price,
+      description,
+    },
+  });
+  res.status(200).json("Product Added");
+});
+
+app.get("/products", async (req, res) => {
+  const data = await prisma.product.findMany();
+  res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
